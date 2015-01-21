c9db65912516279b466c7b5013c35da7fafc4543
rikvdbrule <rikvdbrule@gmail.com>
2015-01-21 12:23:09 +0100

    Build a IP/TCP socket based server instance to play sounds remotely

---
+++ playASoundServer.py
@@ -0,0 +1,61 @@
+import socket
+import pyaudio
+import wave
+
+class PlayASoundServer(object):
+
+    def __init__(self, port = 50007, listners = 1, soundLocation = 'sounds/'):
+        print 'Starting server'
+        self.soundLocation = soundLocation
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.bind(('', port))
+        serverSocket.listen(listners)
+        try:
+            self.conn, self.addr = serverSocket.accept()
+            print 'Connected by ', self.addr
+            self.active = True
+            self.playIncommingSounds()
+        except IOError as e:
+            print "An I/O error occured:\n" + e
+        except Exception as e:
+            print 'An error occured:\n' + e
+        finally:
+            self.close(True)
+
+    def _speechForVirtualNao(self, loc):
+        wf = wave.open(self.soundLocation + loc, 'rb')
+        p = pyaudio.PyAudio()
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+        data = wf.readframes(1024)
+        while data != '':
+            stream.write(data)
+            data = wf.readframes(1024)
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+    def playIncommingSounds(self):
+        while self.active:
+            soundFileName = self.conn.recv(1024)
+            self.conn.send(soundFileName)
+            if soundFileName == 'close':
+                self.active = False
+            elif soundFileName != '':
+                self._speechForVirtualNao(soundFileName)
+        self.close(True)
+
+    def close(self, force = False):
+        self.active = False
+        if force:
+            self.conn.close()
+        print 'Server closed'
+        
+
+if __name__ == "__main__":
+    server = PlayASoundServer()
+
+    
